eaa4264cd9920a3a31e18e9df1e75a6ef36292dc
Mohak Chugh <me.mohakchugh@gmail.com>
2019-10-07 15:54:08 +0530

    manual fetch example added

--- nlp.js
+++ nlp.js
@@ -14,4 +14,22 @@ async function mainExtractEntities() {
     });
 }
 
+async function mainFullExample() {
+    manager.addDocument('en', 'My mail is %email%', 'email');
+    manager.addDocument('en', 'My email is %email%', 'email');
+    manager.addDocument('en', 'Here you have my email: %email%', 'email');
+    manager.addDocument('en', 'Hello', 'greet');
+    manager.addDocument('en', 'Good morning', 'greet');
+    manager.addDocument('en', 'good afternoon', 'greet');
+    manager.addDocument('en', 'good evening', 'greet');
+    manager.addAnswer('en', 'email', 'Your email is {{email}}');
+    manager.addAnswer('en', 'greet', 'Hi!');
+    await manager.train();
+    let result = await manager.process('en', 'I think that my mail is meh@meh.com');
+    console.log(result);
+    result = await manager.process('en', 'Hello bot!');
+    console.log(result);
+  }
+  
+
 mainExtractEntities();
